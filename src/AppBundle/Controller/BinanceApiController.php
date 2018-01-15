3c9ffeca03f6539c6da0e8849fa680728d1a3033
mert simsek <mertsmsk0@gmail.com>
2018-01-15 23:55:29 +0300

    btc price added

---
+++ src/AppBundle/Controller/BinanceApiController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: mert
+ * Date: 1/15/18
+ * Time: 11:19 PM
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Service\BinanceService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class BinanceApiController
+ * @package AppBundle\Controller
+ * @Route("/binance-api/")
+ * @Security("has_role('ROLE_USER')")
+ */
+class BinanceApiController extends Controller
+{
+    /**
+     * @param BinanceService $binanceService
+     * @Route("/user-btc-price", name="binance-api-user-btc-price")
+     * @return JsonResponse
+     */
+    public function getUserBtcPriceAction(BinanceService $binanceService)
+    {
+        $userBtc = $binanceService->getUserBtcPrice($this->getUser());
+        return new JsonResponse(array('userBtc' => $userBtc));
+
+    }
+
+
+}
